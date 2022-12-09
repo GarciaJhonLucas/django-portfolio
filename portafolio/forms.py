f8e82c8f1f2fd939f342680acca64aac4bf5fff8
Grcia Jhon Lucas <garciajhonlucas@gmail.com>
2022-12-08 19:04:13 -0500

    Validacion de formularios
    - Se actualizo los formularios para usar el model
    - Se cambio los formularios del Login, Registro, Proyecto
    - Se cambio los metodos del view, para validar el formulario
    - Se eliminaron las imagenes

---
+++ portafolio/forms.py
@@ -0,0 +1,59 @@
+from django import forms
+from .models import Proyecto
+
+class CreateForm(forms.Form):
+    titulo = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control-input',
+        'id':'ctitulo'
+    }))
+    descripcion = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={
+        'class':'form-control-input',
+        'id':'cdescripcion'
+    }))
+    tags = forms.CharField(
+        required=True, widget=forms.TextInput(attrs={
+        'class':'form-control-input',
+        'id':'ctags'
+    }))
+
+    url_repo = forms.CharField(
+        required=True, widget=forms.URLInput(attrs={
+        'class':'form-control-input',
+        'id':'curlproyecto',
+    }))
+    
+    
+class LoginForm(forms.Form):
+    usuario = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control-input',
+        'id':'cusuario'
+    }))
+    contrasenia = forms.CharField(
+        required=True, widget=forms.PasswordInput(attrs={
+        'class':'form-control-input',
+        'id':'cpassword'
+    }))
+    
+
+class RegisterForm(forms.Form):
+    usuario = forms.CharField(required=True, widget=forms.TextInput(attrs={
+        'class':'form-control-input',
+        'id':'cusuario'
+    }))
+    correo = forms.CharField(
+        required=True, widget=forms.EmailInput(attrs={
+        'class':'form-control-input',
+        'id':'cmail'
+    }))
+    contrasenia = forms.CharField(
+        required=True, widget=forms.PasswordInput(attrs={
+        'class':'form-control-input',
+        'id':'cpassword'
+    }))
+    rep_contrasenia = forms.CharField(
+        required=True, widget=forms.PasswordInput(attrs={
+        'class':'form-control-input',
+        'id':'cpasswords'
+    }))
+    
--- portafolio/views.py
+++ portafolio/views.py
@@ -2,77 +2,105 @@ from django.shortcuts import render, redirect
 from .models import Proyecto
 from django.contrib.auth.models import User, auth
 from django.contrib import messages
-from django.contrib.auth.decorators import login_required
+from django.contrib.auth.decorators import login_required, user_passes_test
+from .forms import CreateForm, LoginForm, RegisterForm
 
 # Create your views here.
 def home(request):
     proyectos = Proyecto.objects.all()
     return render(request, 'index.html', {'proyectos': proyectos} )
 
+
 @login_required(login_url='login')
 def create(request):
+    formulario = CreateForm
+    
     if request.method == 'POST':
-        titulo = request.POST['ctitulo']
-        descripcion = request.POST['cdescripcion']
-        tags = request.POST['ctags']
-        imagen = request.FILES.get('cimagen')
-        url_pryecto = request.POST['curlproyecto']
+        create_form = CreateForm(request.POST)
         
-        if Proyecto.objects.filter(url_github=url_pryecto).exists():
-            messages.warning(request, 'La URL del proyecto ya se registro')
-            return redirect('create')
+        if create_form.is_valid():
+            titulo = request.POST.get('titulo')
+            descripcion = request.POST.get('descripcion')
+            tags = request.POST.get('tags')
+            imagen =  request.FILES['cimagen']
+            url_github = request.POST.get('url_repo')
+            
+            if Proyecto.objects.filter(url_github=url_github).exists():
+                messages.warning(request, 'La URL del proyecto ya se registro')
+                return redirect('create')
+            else:
+                proyecto = Proyecto(foto=imagen, titulo=titulo, descripcion=descripcion, tags=tags, url_github=url_github)
+                proyecto.save()
+                messages.success(request, 'El proyecto se creo correctamente')
+                return redirect('create')
         else:
-            proyecto = Proyecto(foto=imagen, titulo=titulo, descripcion=descripcion, tags=tags, url_github=url_pryecto)
-            proyecto.save()
-            messages.success(request, 'El proyecto se creo correctamente')
+            messages.warning(request, create_form.errors)
             return redirect('create')
     else:
-        return render(request, 'create.html')
+        return render(request, 'create.html', {'forms':formulario})
 
 
+@login_required(login_url='login')
 def logout(request):
     auth.logout(request)
-    return render(request, 'index.html')
+    proyectos = Proyecto.objects.all()
+    return render(request, 'index.html', {'proyectos': proyectos} )
 
 
 def login(request):
+    formulario = LoginForm
+    
     if request.method == 'POST':
-        username = request.POST['cusuario']
-        password = request.POST['cpassword']
+        login_form = LoginForm(request.POST)
         
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('create')
-        else:
-            messages.success(request, 'Los datos son invalidos')
-            return redirect('login')
-    else:
-        return render(request, 'login.html')
+        if login_form.is_valid():
+            username = request.POST.get('usuario')
+            password = request.POST.get('contrasenia')
 
-
-def register(request):
-    if request.method == 'POST':
-        username = request.POST['cusuario']
-        email = request.POST['cmail']
-        password = request.POST['cpassword']
-        password2 = request.POST['cpasswords']
-        
-        if password == password2:
-            if User.objects.filter(email=email).exists():
-                messages.success(request, 'El correo ya se registro')
-                return redirect('register')
-            
-            elif User.objects.filter(username=username).exists():
-                messages.success (request, 'El usuario ya se registro')
-                return redirect('register')
-            
+            user = auth.authenticate(username=username, password=password)
+            if user is not None:
+                auth.login(request, user)
+                return redirect('create')
             else:
-                user = User.objects.create_user(username=username, email=email, password=password)
-                user.save()
+                messages.success(request, 'Los datos son invalidos')
                 return redirect('login')
         else:
-            messages.success (request, 'Las contraseñas no conciden')
-            return redirect('register')
+            messages.warning(request, login_form.errors)
+            return redirect('create')
     else:
-        return render(request, 'register.html')
+        return render(request, 'login.html', {'forms':formulario})
+          
+
+def register(request):
+    formulario = RegisterForm
+     
+    if request.method == 'POST':
+        register_form = RegisterForm(request.POST)
+        
+        if register_form.is_valid():
+            username = request.POST.get('usuario')
+            email = request.POST.get('correo')
+            password = request.POST.get('contrasenia')
+            password2 = request.POST.get('rep_contrasenia')
+
+            if password == password2:
+                if User.objects.filter(email=email).exists():
+                    messages.success(request, 'El correo ya se registro')
+                    return redirect('register')
+
+                elif User.objects.filter(username=username).exists():
+                    messages.success (request, 'El usuario ya se registro')
+                    return redirect('register')
+
+                else:
+                    user = User.objects.create_user(username=username, email=email, password=password)
+                    user.save()
+                    return redirect('login')
+            else:
+                messages.success (request, 'Las contraseñas no conciden')
+                return redirect('register')
+        else:
+            messages.warning(request, register_form.errors)
+            return redirect('create')
+    else:
+        return render(request, 'register.html', {'forms':formulario})
