6d9fe11c23bbddaa5522d1c15e4b7f3da97e30e6
Grcia Jhon Lucas <garciajhonlucas@gmail.com>
2022-12-11 21:24:25 -0500

    Railway Settings v4.0

--- mainapp/settings.py
+++ mainapp/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'portafolio',
 ]
 
+CSRF_TRUSTED_ORIGINS = ['https://django-portfolio-production-0e79.up.railway.app/']
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
